db6cb3b314f34b1d13392283d2cb477e0dad7921
Alex Zaitsev <saizewalex@gmail.com>
2023-03-19 21:26:17 +0300

    All tests included

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MoneyTransferTest {
     @BeforeEach
     void loginToAccount() {
-        Configuration.holdBrowserOpen = true;
+        // Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
@@ -27,7 +27,7 @@ class MoneyTransferTest {
     @Test
     @DisplayName("Should transfer money from the first to the second card")
     void shouldTransferFrom1to2card() {
-        Configuration.holdBrowserOpen = true;
+        // Configuration.holdBrowserOpen = true;
         var transferAmount = 2000;
         var dashboardPage = new DashboardPage();
         var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
@@ -44,7 +44,7 @@ class MoneyTransferTest {
     @Test
     @DisplayName("Should transfer money from the second to the first card")
     void shouldTransferFrom2to1card() {
-        Configuration.holdBrowserOpen = true;
+       // Configuration.holdBrowserOpen = true;
         var transferAmount = 1500;
         var dashboardPage = new DashboardPage();
         var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
@@ -57,52 +57,52 @@ class MoneyTransferTest {
         assertEquals(initialBalanceFirstCard + transferAmount, firstCardBalance);
         assertEquals(initialBalanceSecondCard - transferAmount, secondCardBalance);
     }
-//
-//    @Test
-//    @DisplayName("Should not transfer from first card an amount greater than the balance of the first card")
-//    void shouldNotTransferAmountGreaterFirsCardBalance() {
-//        Configuration.holdBrowserOpen = true;
-//        var transferAmount = 55_000;
-//        var dashboardPage = new DashboardPage();
-//        var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
-//        var initialBalanceSecondCard = dashboardPage.getCardBalance(DataHelper.getSecondCard().getId());
-//        dashboardPage.replenishmentSecondCard();
-//        var cardReplenishmentPage = new CardReplenishmentPage();
-//        cardReplenishmentPage.transferFromCardToCard(String.valueOf(transferAmount), DataHelper.getFirstCard());
-//        cardReplenishmentPage.waitingError();
-//        assertEquals(initialBalanceFirstCard, dashboardPage.getCardBalance(DataHelper.getFirstCard().getId()));
-//        assertEquals(initialBalanceSecondCard, dashboardPage.getCardBalance(DataHelper.getSecondCard().getId()));
-//    }
-//
-//    @Test
-//    @DisplayName("Should not transfer from first card an amount greater than the balance of the second card")
-//    void shouldNotTransferAmountGreaterSecondCardBalance() {
-//        Configuration.holdBrowserOpen = true;
-//        var transferAmount = 155_000;
-//        var dashboardPage = new DashboardPage();
-//        var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
-//        var initialBalanceSecondCard = dashboardPage.getCardBalance(DataHelper.getSecondCard().getId());
-//        dashboardPage.replenishmentFirstCard();
-//        var cardReplenishmentPage = new CardReplenishmentPage();
-//        cardReplenishmentPage.transferFromCardToCard(String.valueOf(transferAmount), DataHelper.getSecondCard());
-//        cardReplenishmentPage.waitingError();
-//        assertEquals(initialBalanceFirstCard, dashboardPage.getCardBalance(DataHelper.getFirstCard().getId()));
-//        assertEquals(initialBalanceSecondCard, dashboardPage.getCardBalance(DataHelper.getSecondCard().getId()));
-//    }
-//
-//    @Test
-//    @DisplayName("Should not transfer from the first card if the amount field is empty")
-//    void shouldNotTransferFromFirstCardIfAmountEmpty() {
-//        Configuration.holdBrowserOpen = true;
-//        var dashboardPage = new DashboardPage();
-//        var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
-//        var initialBalanceSecondCard = dashboardPage.getCardBalance(DataHelper.getSecondCard().getId());
-//        dashboardPage.replenishmentFirstCard();
-//        var cardReplenishmentPage = new CardReplenishmentPage();
-//        cardReplenishmentPage.transferWithoutAmount(DataHelper.getSecondCard());
-//        cardReplenishmentPage.waitingError();
-//        assertEquals(initialBalanceFirstCard, dashboardPage.getCardBalance(DataHelper.getFirstCard().getId()));
-//        assertEquals(initialBalanceSecondCard, dashboardPage.getCardBalance(DataHelper.getSecondCard().getId()));
-//    }
+
+    @Test
+    @DisplayName("Should not transfer from first card an amount greater than the balance of the first card")
+    void shouldNotTransferAmountGreaterFirsCardBalance() {
+       // Configuration.holdBrowserOpen = true;
+        var transferAmount = 55_000;
+        var dashboardPage = new DashboardPage();
+        var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
+        var initialBalanceSecondCard = dashboardPage.getCardBalance(DataHelper.getSecondCard().getId());
+        dashboardPage.replenishmentSecondCard();
+        var cardReplenishmentPage = new CardReplenishmentPage();
+        cardReplenishmentPage.transferFromCardToCard(String.valueOf(transferAmount), DataHelper.getFirstCard());
+        cardReplenishmentPage.waitingError();
+        assertEquals(initialBalanceFirstCard, dashboardPage.getCardBalance(DataHelper.getFirstCard().getId()));
+        assertEquals(initialBalanceSecondCard, dashboardPage.getCardBalance(DataHelper.getSecondCard().getId()));
+    }
+
+    @Test
+    @DisplayName("Should not transfer from first card an amount greater than the balance of the second card")
+    void shouldNotTransferAmountGreaterSecondCardBalance() {
+      //  Configuration.holdBrowserOpen = true;
+        var transferAmount = 155_000;
+        var dashboardPage = new DashboardPage();
+        var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
+        var initialBalanceSecondCard = dashboardPage.getCardBalance(DataHelper.getSecondCard().getId());
+        dashboardPage.replenishmentFirstCard();
+        var cardReplenishmentPage = new CardReplenishmentPage();
+        cardReplenishmentPage.transferFromCardToCard(String.valueOf(transferAmount), DataHelper.getSecondCard());
+        cardReplenishmentPage.waitingError();
+        assertEquals(initialBalanceFirstCard, dashboardPage.getCardBalance(DataHelper.getFirstCard().getId()));
+        assertEquals(initialBalanceSecondCard, dashboardPage.getCardBalance(DataHelper.getSecondCard().getId()));
+    }
+
+    @Test
+    @DisplayName("Should not transfer from the first card if the amount field is empty")
+    void shouldNotTransferFromFirstCardIfAmountEmpty() {
+       // Configuration.holdBrowserOpen = true;
+        var dashboardPage = new DashboardPage();
+        var initialBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCard().getId());
+        var initialBalanceSecondCard = dashboardPage.getCardBalance(DataHelper.getSecondCard().getId());
+        dashboardPage.replenishmentFirstCard();
+        var cardReplenishmentPage = new CardReplenishmentPage();
+        cardReplenishmentPage.transferWithoutAmount(DataHelper.getSecondCard());
+        cardReplenishmentPage.waitingError();
+        assertEquals(initialBalanceFirstCard, dashboardPage.getCardBalance(DataHelper.getFirstCard().getId()));
+        assertEquals(initialBalanceSecondCard, dashboardPage.getCardBalance(DataHelper.getSecondCard().getId()));
+    }
 
 }
